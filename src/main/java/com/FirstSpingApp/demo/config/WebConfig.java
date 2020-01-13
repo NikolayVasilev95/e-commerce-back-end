e2a821a708c8d0b109e51d2a2dffbc6f3031073d
Nikolay Vasilev <metalistite_two@abv.bg>
2020-01-13 10:09:13 +0200

    Corss security connection

--- src/main/java/com/FirstSpingApp/demo/config/WebConfig.java
+++ src/main/java/com/FirstSpingApp/demo/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://e-commerce-dev-front.herokuapp.com").allowedMethods("*").allowedHeaders("*").allowCredentials(true).maxAge(3600);
+        registry.addMapping("/api/**").allowedOrigins("https://e-commerce-dev-front.herokuapp.com").allowedMethods("*").allowedHeaders("*").allowCredentials(true).maxAge(3600);
     }
 
 }
